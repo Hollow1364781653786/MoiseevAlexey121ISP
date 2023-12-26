6b140398c12504dd29214af2e19d52cc6188d505
YoursTrulyHollow <100510026+Hollow1364781653786@users.noreply.github.com>
2023-12-26 11:21:44 +0700

    Авторизация через xml файл в windows forms
    Программа, выполняющая запись и чтение данных в xml файле на windows forms

---
+++ WindowsFormsAppAuthorisation/WindowsFormsAppAuthorisation/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsAppAuthorisation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginregbox = new System.Windows.Forms.TextBox();
+            this.loginenbox = new System.Windows.Forms.TextBox();
+            this.passregbox = new System.Windows.Forms.TextBox();
+            this.passenbox = new System.Windows.Forms.TextBox();
+            this.enterbutton = new System.Windows.Forms.Button();
+            this.registerbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вход";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Регистрация";
+            // 
+            // loginregbox
+            // 
+            this.loginregbox.Location = new System.Drawing.Point(267, 100);
+            this.loginregbox.Name = "loginregbox";
+            this.loginregbox.Size = new System.Drawing.Size(100, 20);
+            this.loginregbox.TabIndex = 2;
+            // 
+            // loginenbox
+            // 
+            this.loginenbox.Location = new System.Drawing.Point(12, 100);
+            this.loginenbox.Name = "loginenbox";
+            this.loginenbox.Size = new System.Drawing.Size(100, 20);
+            this.loginenbox.TabIndex = 3;
+            // 
+            // passregbox
+            // 
+            this.passregbox.Location = new System.Drawing.Point(267, 167);
+            this.passregbox.Name = "passregbox";
+            this.passregbox.Size = new System.Drawing.Size(100, 20);
+            this.passregbox.TabIndex = 4;
+            // 
+            // passenbox
+            // 
+            this.passenbox.Location = new System.Drawing.Point(12, 167);
+            this.passenbox.Name = "passenbox";
+            this.passenbox.Size = new System.Drawing.Size(100, 20);
+            this.passenbox.TabIndex = 5;
+            // 
+            // enterbutton
+            // 
+            this.enterbutton.Location = new System.Drawing.Point(24, 215);
+            this.enterbutton.Name = "enterbutton";
+            this.enterbutton.Size = new System.Drawing.Size(75, 23);
+            this.enterbutton.TabIndex = 6;
+            this.enterbutton.Text = "button1";
+            this.enterbutton.UseVisualStyleBackColor = true;
+            this.enterbutton.Click += new System.EventHandler(this.enterbutton_Click);
+            // 
+            // registerbutton
+            // 
+            this.registerbutton.Location = new System.Drawing.Point(278, 215);
+            this.registerbutton.Name = "registerbutton";
+            this.registerbutton.Size = new System.Drawing.Size(75, 23);
+            this.registerbutton.TabIndex = 7;
+            this.registerbutton.Text = "button2";
+            this.registerbutton.UseVisualStyleBackColor = true;
+            this.registerbutton.Click += new System.EventHandler(this.registerbutton_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 271);
+            this.Controls.Add(this.registerbutton);
+            this.Controls.Add(this.enterbutton);
+            this.Controls.Add(this.passenbox);
+            this.Controls.Add(this.passregbox);
+            this.Controls.Add(this.loginenbox);
+            this.Controls.Add(this.loginregbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox loginregbox;
+        private System.Windows.Forms.TextBox loginenbox;
+        private System.Windows.Forms.TextBox passregbox;
+        private System.Windows.Forms.TextBox passenbox;
+        private System.Windows.Forms.Button enterbutton;
+        private System.Windows.Forms.Button registerbutton;
+    }
+}
+
---
+++ WindowsFormsAppAuthorisation/WindowsFormsAppAuthorisation/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+
+namespace WindowsFormsAppAuthorisation
+{
+    public partial class Form1 : Form
+    {
+        public string defpath = "users.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(defpath)) CreateXMLDocument(defpath);
+        }
+
+        private void CreateXMLDocument(string filepath) //создание xml файла
+        {
+            XmlTextWriter xtw = new XmlTextWriter(filepath, Encoding.UTF32);
+            xtw.WriteStartDocument();
+            xtw.WriteStartElement("users");
+            xtw.WriteEndDocument();
+            xtw.Close();
+        }
+
+        private string MaxID(string filepath) // вычисление ID, которое можно занять
+        {
+            List<int> idList = new List<int>();
+            int id;
+            XmlDocument xd = new XmlDocument();
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            xd.Load(fs);
+            XmlNodeList list = xd.GetElementsByTagName("user");
+            if (list.Count > 0)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    XmlElement user = (XmlElement)xd.GetElementsByTagName("user")[i];
+                    id = Convert.ToInt32(user.GetAttribute("id"));
+                    idList.Add(id);
+                }
+                int result = 0;
+                foreach (int j in idList)
+                    if (j > result)
+                        result = j;
+                result++;
+                fs.Close();
+                return result.ToString();
+            }
+            else
+            {
+                fs.Close();
+                return "1";
+            }
+        }
+
+        private void WriteToXMLDocument(string filepath, string name, string pwd) //добавление записи
+        {
+            if (!File.Exists(defpath)) CreateXMLDocument(defpath);
+            string id = MaxID(filepath);
+            XmlDocument xd = new XmlDocument();
+            xd.PreserveWhitespace = false;
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            xd.Load(fs);
+
+            XmlElement user = xd.CreateElement("user");
+            user.SetAttribute("id", id);
+
+            XmlElement login = xd.CreateElement("login");
+            XmlElement pass = xd.CreateElement("password");
+
+            XmlText tLogin = xd.CreateTextNode(name);
+            XmlText tPassword = xd.CreateTextNode(pwd);
+
+            login.AppendChild(tLogin);
+            pass.AppendChild(tPassword);
+
+            user.AppendChild(login);
+            user.AppendChild(pass);
+
+            xd.DocumentElement.AppendChild(user);
+
+            fs.Close();
+            xd.Save(filepath);
+        }
+
+        private void registerbutton_Click_1(object sender, EventArgs e)
+        {
+            if (!(loginregbox.Text == "") & !(passregbox.Text == ""))
+                WriteToXMLDocument(defpath, loginregbox.Text, passregbox.Text);
+            else MessageBox.Show("Введите имя пользователя и пароль");
+        }
+
+        private void ReadXMLDocument(string filepath, string loget, string passget)
+        {
+            string name, pwd;
+            XmlDocument xd = new XmlDocument();
+            xd.PreserveWhitespace = false;
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            xd.Load(fs);
+            XmlNodeList list = xd.GetElementsByTagName("user");
+            for (int i = 0; i < list.Count; i++)
+            {
+                XmlElement user = (XmlElement)xd.GetElementsByTagName("login")[i];
+                XmlElement pass = (XmlElement)xd.GetElementsByTagName("password")[i];
+                name = user.InnerText;
+                pwd = pass.InnerText;
+                if ((loget == name) & (passget == pwd))
+                {
+                    Form2 form2 = new Form2(user.InnerText);
+                    form2.Data = user.InnerText;
+                    form2.Data2 = pass.InnerText;
+                    form2.Show();
+                    break;
+                }
+                else if (i == list.Count - 1) MessageBox.Show("Неверный логин или пароль");
+            }
+            fs.Close();
+        }
+
+        private void enterbutton_Click(object sender, EventArgs e)
+        {
+            if (!(loginenbox.Text == "") & !(passenbox.Text == ""))
+            {
+                ReadXMLDocument(defpath, loginenbox.Text, passenbox.Text);
+            }
+        }
+    }
+}
